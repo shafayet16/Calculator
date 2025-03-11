eaff79ebab68a5d580429c4859c3d5cb6520c5f8
shafayet16 <hirasan893@gmail.com>
2025-03-11 16:25:42 +0600

    Add initial JavaScript file for calculator functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+// Result Display Elements
+const resultDisplay = document.getElementById("result");
+const answerDisplay = document.getElementById("answer");
+
+// Operation Buttons
+const button1 = document.getElementById("1");
+const button2 = document.getElementById("2");
+const button3 = document.getElementById("3");
+const buttonPlus = document.getElementById("+");
+
+const button4 = document.getElementById("4");
+const button5 = document.getElementById("5");
+const button6 = document.getElementById("6");
+const buttonMinus = document.getElementById("-");
+
+const button7 = document.getElementById("7");
+const button8 = document.getElementById("8");
+const button9 = document.getElementById("9");
+const buttonMultiply = document.getElementById("*");
+
+const button0 = document.getElementById("0");
+const buttonClear = document.getElementById("clear");
+const buttonEquals = document.getElementById("=");
+const buttonDivide = document.getElementById("/");
